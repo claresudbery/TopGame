c3bccdf17481809679462db7fd60915d30011a02
Clare Sudbery <clare.sudbery@nice.org.uk>
2016-10-24 18:58:49 +0100

    Trying to work out why golden master content has changed.

--- UnitTests/GoldenMasterTests.cs
+++ UnitTests/GoldenMasterTests.cs
@@ -17,6 +17,32 @@ namespace UnitTests
         [Test]
         public void Latest_calculated_data_should_be_the_same_as_stored_golden_master_data()
         {
+            // Arrange & Act
+            var topGameAppPath = TestContext.CurrentContext.TestDirectory + @"..\..\..\GoldenMasters\";
+            GoldenMasterList goldenMaster002 = TopGameJsonWriter.ReadFromJsonFile<GoldenMasterList>(topGameAppPath + "TopGame-GoldenMaster-002.json");
+            string goldenMaster002AsJsonString;
+            using (StreamReader file = File.OpenText(topGameAppPath + "TopGame-GoldenMaster-002.json"))
+            {
+                goldenMaster002AsJsonString = file.ReadToEnd();
+            }
+            GoldenMasterList goldenMaster003 = TopGameJsonWriter.ReadFromJsonFile<GoldenMasterList>(topGameAppPath + "TopGame-GoldenMaster-003.json");
+            string goldenMaster003AsJsonString;
+            using (StreamReader file = File.OpenText(topGameAppPath + "TopGame-GoldenMaster-003.json"))
+            {
+                goldenMaster003AsJsonString = file.ReadToEnd();
+            }
+
+            // Assert
+            goldenMaster002.GoldenMasters.Count.Equals(goldenMaster003.GoldenMasters.Count);
+            goldenMaster002.GoldenMasters[0].ShouldBeEquivalentTo(goldenMaster003.GoldenMasters[0]);
+            goldenMaster002.GoldenMasters[300].ShouldBeEquivalentTo(goldenMaster003.GoldenMasters[300]);
+            goldenMaster002.GoldenMasters[goldenMaster002.GoldenMasters.Count - 1].ShouldBeEquivalentTo(goldenMaster003.GoldenMasters[goldenMaster003.GoldenMasters.Count - 1]);
+            Assert.That(goldenMaster002AsJsonString, Is.EqualTo(goldenMaster003AsJsonString));
+        }
+
+        [Test]
+        public void Version_002_should_be_the_same_as_version_003()
+        {
             // Arrange
             var topGameAppPath = TestContext.CurrentContext.TestDirectory + @"..\..\..\GoldenMasters\";
             GoldenMasterList storedGoldenMaster = TopGameJsonWriter.ReadFromJsonFile<GoldenMasterList>(topGameAppPath + _currentGoldenMasterFileName);
@@ -25,7 +51,7 @@ namespace UnitTests
             {
                 storedGoldenMasterAsJsonString = file.ReadToEnd();
             }
-            var graphicLoops = new List<OnePlayerGraphicsLoop> { new OnePlayerGraphicsLoop() };
+            var graphicLoops = CreateGraphicLoops();//new List<OnePlayerGraphicsLoop> { new OnePlayerGraphicsLoop() };
 
             // Act
             var latestCalculatedData = GoldenMasterPopulator.GenerateAllData(graphicLoops);
@@ -62,6 +88,27 @@ namespace UnitTests
             latestGoldenMaster.GoldenMasters[300].ShouldBeEquivalentTo(goldenMaster002.GoldenMasters[300]);
             latestGoldenMaster.GoldenMasters[latestGoldenMaster.GoldenMasters.Count - 1].ShouldBeEquivalentTo(goldenMaster002.GoldenMasters[latestGoldenMaster.GoldenMasters.Count - 1]);
             Assert.That(latestGoldenMasterAsJsonString, Is.EqualTo(goldenMaster002AsJsonString));
+        }
+
+        private List<OnePlayerGraphicsLoop> CreateGraphicLoops()
+        {
+            var allGraphicLoops = new List<OnePlayerGraphicsLoop>
+            {
+                new OnePlayerGraphicsLoop(),
+                new OnePlayerGraphicsLoop(),
+                new OnePlayerGraphicsLoop()
+            };
+
+            foreach (var graphicLoop in allGraphicLoops)
+            {
+                graphicLoop.Clear();
+                for (int count = 1; count <= 26; count++)
+                {
+                    graphicLoop.AddSegment();
+                }
+            }
+
+            return allGraphicLoops;
         }
     }
 }
